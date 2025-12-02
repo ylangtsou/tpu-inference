746cd465d6f8625d644a4ee6389adc7f02e66e7d
Kyuyeun Kim <kyuyeunk@google.com>
2025-12-02 11:53:57 +0000

    tweak rpa tiles
    Signed-off-by: Kyuyeun Kim <kyuyeunk@google.com>

--- tpu_inference/kernels/ragged_paged_attention/v3/kernel_hd64.py
+++ tpu_inference/kernels/ragged_paged_attention/v3/kernel_hd64.py
@@ -1463,6 +1463,12 @@ def ragged_paged_attention_hd64(
             max_num_tokens,
             pages_per_seq,
         )
+
+    bq_sz = 16
+    bkv_p = 16
+    # if sliding_window is not None:
+    #     bkv_p = 4
+
     bkv_sz = bkv_p * page_size
     if vmem_limit_bytes is None:
         # TODO (jevinjiang/jacobplatin): change this to use
