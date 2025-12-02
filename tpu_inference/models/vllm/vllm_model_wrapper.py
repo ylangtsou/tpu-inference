56f03b371ea955ace50e96b2679928813abc8bae
Kyuyeun Kim <kyuyeunk@google.com>
2025-12-02 05:35:51 +0000

    [DO NOT SUBMIT] Disable sparse core all reduce
    Signed-off-by: Kyuyeun Kim <kyuyeunk@google.com>

--- tpu_inference/models/vllm/vllm_model_wrapper.py
+++ tpu_inference/models/vllm/vllm_model_wrapper.py
@@ -158,7 +158,9 @@ class VllmModelWrapper:
                 "xla_tpu_all_gather_collective_matmul_mode":
                 "post_spmd_conservative",
                 "xla_tpu_reduce_scatter_collective_matmul_mode":
-                "post_spmd_conservative"
+                "post_spmd_conservative",
+                "xla_tpu_enable_sparse_core_collective_offload_all_reduce":
+                False,
             },
             static_argnames=("layer_name_to_kvcache_index", "is_first_rank",
                              "is_last_rank"),
